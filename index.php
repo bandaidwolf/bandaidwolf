5963c99a6d1c810e47858eef25b3e90dd05e103d
bandaidwolf <43256826+bandaidwolf@users.noreply.github.com>
2018-09-13 18:41:07 -0700

    asdf

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-
+<DOCTYPE! html>
+<html>
+  <head> 
+    <title>Interveiw of a bandaid box survivor</title> 
+  </head>
+  <body>
+    <h1>Projects</h1>
+    <a href=¨/projects/1.php¨ target=¨_blank¨>My first project!</a>
+  </body>
+ </html>
