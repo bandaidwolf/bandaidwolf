cb656e093f5bf71d36bc3363082d4741e3272aa1
stelladraco27 <stelladraco27@gmail.com>
2018-09-16 17:35:55 -0700

    asdf

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <DOCTYPE! html>
 <html>
-  <head> 
-    <title>Interveiw of a bandaid box survivor</title> 
+  <head>
+    <title>Interveiw of a bandaid box survivor</title>
   </head>
   <body>
     <h1>Projects</h1>
     <a href="/projects/1.php" target="_blank">My first project!</a>
+    <div>
+      <h1>Lol, somebody waz here</h1>
+    </div>
   </body>
  </html>
